fde97a62b70d43a7874693dad55ffc69b9e9167b
uramer <antonuramer@gmail.com>
2020-11-16 18:51:54 +0100

    Update icon and D6

--- Res/build.ps1
+++ Res/build.ps1
@@ -1,4 +1,17 @@
+function ToPng($name) {
+	$from = "$name.xcf"
+	$to = "$name.png"
+	magick convert -background none $from -flatten $to
+}
+
+Get-ChildItem "." -Filter *.xcf | 
+Foreach-Object {
+    $name = $_.Basename
+	ToPng $name
+}
+
 Copy-Item "*.png" -Destination "../Dice/res" -Force
 
 cd ./Dice
 ./build.ps1
+cd ..
